54bd657562e64c529fb6187561e7eeae2fb78884
amialone <68087960+amialone@users.noreply.github.com>
2020-09-24 15:52:46 +1200

    Create username-password.sh
    check username and password

---
+++ username-password.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+echo "Enter the username"
+read username
+
+echo "Enter the password"
+read password
+
+retry=1
+
+if [ -n "$username" ] && [ -n "$password" ]
+then
+        if [ "$username" == "admin" ] && [ "$password" == "password" ]
+                then
+                        echo "login success"
+                else
+                        while [ $retry -lt 3 ]
+                        do
+                                echo "Username or password incorect"
+                                echo "Enter the username"
+                                read username
+
+                                echo "Enter the password"
+                                read password
+                                if [ "$username" == "admin" ] && [ "$password" == "password" ]
+                                then
+                                        echo "login success"
+                                        break
+                                else
+                                        ((retry++))
+                                fi
+                        done
+                fi
+else
+        echo "username and password field cannot be empty"
+fi
+
