d0a2a0c57cdd17b851eb2bed3acab2bd5858e073
amialone <68087960+amialone@users.noreply.github.com>
2020-09-24 15:55:09 +1200

    Update username-password.sh

--- username-password.sh
+++ username-password.sh
@@ -34,4 +34,43 @@ then
 else
         echo "username and password field cannot be empty"
 fi
+--------------------------------------------------------------------------------------------------------------------------------------------------------------
+With Functions 
+#!/bin/bash
+
+hrishi() {
+
+        echo "Enter the username"
+        read username
+
+        echo "Enter the password"
+        read password
+}
+hrishi
+
+retry=1
+
+if [ -n "$username" ] && [ -n "$password" ]
+then
+        if [ "$username" == "admin" ] && [ "$password" == "password" ]
+                then
+                        echo "login success"
+                else
+                        while [ $retry -lt 3 ]
+                        do
+                                echo "Username or password incorect"
+                                hrishi
+                                if [ "$username" == "admin" ] && [ "$password" == "password" ]
+                                then
+                                        echo "login success"
+                                        break
+                                else
+                                        ((retry++))
+                                fi
+                        done
+                fi
+else
+        echo "username and password field cannot be empty"
+fi
+
 
