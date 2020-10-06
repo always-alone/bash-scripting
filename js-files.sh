ec110a77a510219019202e2e0a4243a6bbc63da9
amialone <68087960+amialone@users.noreply.github.com>
2020-10-06 14:13:19 +1300

    Update js-files.sh

--- js-files.sh
+++ js-files.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
+#Name: Hrishikesh Ghosalkar
+#Bash-Script: To find JavaScript files 
+#For OSCP lab excerises
 
 echo "Enter the file location: ABSOLUTE PATH"
-read file 
+read location
+
+echo "*******Files Found*******"
+ls -l $file 2>/dev/null
+echo "*******Files Found*******"
+
+echo "Enter the file name"
+read file
 
 rm -rf /home/kali/bash-scripts/js-files
-if [ -n "$file" ]
+if [ -n "$locatin$file" ]
 then
-        if [ -f "$file" ]
+        if [ -f "$location$file" ]
         then
-                cat $file | cut -d "/" -f5 | grep -i ".js" | cut -d " " -f1 | sort | uniq 1>/home/kali/bash-scripts/js-files
+                echo "Trying to find JavaScript Files in $ilocation$file"
+                cat $location$file | cut -d "/" -f5 | grep -i ".js" | cut -d " " -f1 | sort | uniq 1> /home/kali/bash-scripts/js-files
 
                 if [ -s "/home/kali/bash-scripts/js-files" ]
                 then
@@ -19,9 +30,7 @@ then
                 fi
         else
                 echo "File doesnt exist"
-        fi      
+        fi
 else
         echo "Filed cannot be empty"
 fi
-
-
